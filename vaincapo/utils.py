cf56a4c3668deef58c5e67d0d6d394f7c74e2834
Fereidoon Zangeneh <efreidun@gmail.com>
2022-05-04 21:35:05 +0200

    Add conversion function from quat to hopf

--- vaincapo/utils.py
+++ vaincapo/utils.py
@@ -7,6 +7,40 @@ from scipy.spatial.transform import Rotation
 import torch
 
 
+def quat_to_hopf(quat: np.ndarray) -> np.ndarray:
+    """Convert quaternion to hopf coordinates.
+
+    Args:
+        quaternion: unit quaternions in [w, x, y, z], shape (N, 4)
+
+    Returns:
+        hopf coordinates psi, theta, phi in [0, 2pi), [0, pi], [0, 2pi), shape (N, 3)
+    """
+    w, x, y, z = quat.T
+    psi = 2 * np.arctan2(x, w)
+    theta = 2 * np.arctan2(np.sqrt(z ** 2 + y ** 2), np.sqrt(w ** 2 + x ** 2))
+    phi = np.arctan2(z * w - x * y, y * w + x * z)
+
+    # Note for the following correction use while instead of if, to support
+    # float32, because atan2 range for float32 ([-np.float32(np.pi),
+    # np.float32(np.pi)]) is larger than for float64 ([-np.pi,np.pi]).
+
+    # Psi must be [0, 2pi) and wraps around at 4*pi, so this correction changes the
+    # the half-sphere
+    while np.any(psi < 0):
+        psi[psi < 0] += 2 * np.pi
+    while np.any(psi >= 2 * np.pi):
+        psi[psi >= 2 * np.pi] -= 2 * np.pi
+
+    # Phi must be [0, 2pi) and wraps around at 2*pi, so this correction just makes
+    # sure the angle is in the expected range
+    while np.any(phi < 0):
+        phi[phi < 0] += 2 * np.pi
+    while np.any(phi >= 2 * np.pi):
+        phi[phi >= 2 * np.pi] -= 2 * np.pi
+    return np.vstack((psi, theta, phi)).T
+
+
 def chordal_to_geodesic(dist: torch.Tensor, deg: bool = False) -> torch.Tensor:
     """Convert rotation chordal distance to geodesic distance.
 
