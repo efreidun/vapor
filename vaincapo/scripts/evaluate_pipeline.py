45121257038d4da264cd7ec4eec577fe11dbb2ee
Fereidoon Zangeneh <efreidun@gmail.com>
2022-05-11 22:43:17 +0200

    Add batch support to evaluation pipeline

--- vaincapo/scripts/evaluate_pipeline.py
+++ vaincapo/scripts/evaluate_pipeline.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 from types import SimpleNamespace
 import yaml
-from glob import glob
 
 import argparse
 import torch
@@ -33,7 +32,8 @@ def parse_arguments() -> dict:
     )
     parser.add_argument("--run", type=str)
     parser.add_argument("--epoch", type=int, default=None)
-    parser.add_argument("--num_samples", type=int, default=1000)
+    parser.add_argument("--batch_size", type=int, default=None)
+    parser.add_argument("--num_samples", type=int, default=10000)
     parser.add_argument("--num_workers", type=int, default=4)
     args = parser.parse_args()
 
@@ -42,9 +42,9 @@ def parse_arguments() -> dict:
 
 def main(config: dict) -> None:
     recall_thresholds = [[0.1, 10.0], [0.2, 15.0], [0.3, 20.0], [1.0, 60.0]]
-    kde_gaussian_sigmas = np.linspace(0.01, 1.0, num=100, endpoint=True)
-    kde_bingham_lambdas = np.linspace(0.1, 1500.0, num=100, endpoint=True)
-    recall_min_samples = [1, 5, 10, 15, 20, 25, 50, 75, 100, 250, 500]
+    kde_gaussian_sigmas = np.linspace(0.01, 0.50, num=100, endpoint=True)
+    kde_bingham_lambdas = np.linspace(200.0, 300.0, num=100, endpoint=True)
+    recall_min_samples = [1, 25, 50, 100, 150, 200, 300, 400, 500]
 
     cfg = SimpleNamespace(**config)
     base_path = Path.home() / "code" / "vaincapo"
@@ -65,8 +65,8 @@ def main(config: dict) -> None:
     )
 
     if cfg.epoch is None:
-        encoder_path = sorted(glob(str(run_path / "encoder_*.pth")))[-1]
-        posemap_path = sorted(glob(str(run_path / "posemap_*.pth")))[-1]
+        encoder_path = sorted(run_path.glob("encoder_*.pth"))[-1]
+        posemap_path = sorted(run_path.glob("posemap_*.pth"))[-1]
     else:
         encoder_path = run_path / f"encoder_{cfg.epoch}.pth"
         posemap_path = run_path / f"posemap_{cfg.epoch}.pth"
@@ -86,20 +86,34 @@ def main(config: dict) -> None:
     datasets = {"train": train_set, "valid": valid_set}
     for dataset in datasets.values():
         dataloader = DataLoader(
-            dataset, batch_size=len(dataset), num_workers=cfg.num_workers
+            dataset,
+            batch_size=cfg.batch_size or len(dataset),
+            num_workers=cfg.num_workers,
         )
-        data = next(iter(dataloader))
-        tra, rot, tra_hat, rot_hat, _, _, _, _, = forward_pass(
-            encoder,
-            posemap,
-            data,
-            cfg.num_samples,
-            train_cfg.num_winners,
-            train_cfg.tra_weight,
-            train_cfg.rot_weight,
-            scene_dims,
-            device,
-        )
+        tras = []
+        rots = []
+        tra_hats = []
+        rot_hats = []
+        for batch in dataloader:
+            tra, rot, tra_hat, rot_hat, _, _, _, _, = forward_pass(
+                encoder,
+                posemap,
+                batch,
+                cfg.num_samples,
+                train_cfg.num_winners,
+                train_cfg.tra_weight,
+                train_cfg.rot_weight,
+                scene_dims,
+                device,
+            )
+            tras.append(tra)
+            rots.append(rot)
+            tra_hats.append(tra_hat)
+            rot_hats.append(rot_hat)
+        tra = torch.cat(tras)
+        tra_hat = torch.cat(tra_hats)
+        rot = torch.cat(rots)
+        rot_hat = torch.cat(rot_hats)
         rot_quat = torch.tensor(rotmat_to_quat(rot.cpu().numpy()))
         rot_quat_hat = torch.tensor(
             rotmat_to_quat(rot_hat.reshape(-1, 3, 3).cpu().numpy())
@@ -169,6 +183,10 @@ def main(config: dict) -> None:
     results_matrix = np.concatenate((loglik_matrix, recall_matrix), axis=1)
 
     with open(run_path / "metrics.txt", "w") as f:
+        f.write(
+            f"{cfg.run} epoch {encoder_path.stem.split('_')[1]}"
+            + f" with {cfg.num_samples} samples\n"
+        )
         for row in results_matrix:
             f.write("\t".join([val.ljust(6) for val in row]) + "\n")
 
