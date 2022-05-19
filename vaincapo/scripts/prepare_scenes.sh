77e802ad1dfcb9a30b3e00a04c028ecc5ea7851b
Fereidoon Zangeneh <efreidun@gmail.com>
2022-05-19 08:40:24 +0200

    Add bash script to prepare scenes for ingp

---
+++ vaincapo/scripts/prepare_scenes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+bash_script_path=$(realpath $0)
+scripts_dir_path=$(dirname $bash_script_path)
+
+for scene in "meeting_table" "blue_chairs" "seminar" "staircase" "staircase_ext"
+do
+    echo "starting scene $scene"
+    python $scripts_dir_path/prepare_scene_json.py $scene
+done
