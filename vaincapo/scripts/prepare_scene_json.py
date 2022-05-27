030ec141a7b79d70cdf7ae7e6c7fcf3c439c3808
Fereidoon Zangeneh <efreidun@gmail.com>
2022-05-27 12:48:17 +0200

    Add support for 7Scenes in iNGP scene preparation

--- vaincapo/scripts/prepare_scene_json.py
+++ vaincapo/scripts/prepare_scene_json.py
@@ -12,7 +12,7 @@ import numpy as np
 from tqdm import tqdm
 
 from vaincapo.utils import get_ingp_transform
-from vaincapo.read_write import read_poses
+from vaincapo.read_write import read_poses, read_tfmat
 
 
 def parse_arguments() -> dict:
@@ -23,6 +23,7 @@ def parse_arguments() -> dict:
     """
     parser = argparse.ArgumentParser(description="Write JSON file for a scene.")
     parser.add_argument("scene", type=str)
+    parser.add_argument("--dataset", type=str, default="AmbiguousReloc")
     parser.add_argument("--split", type=str, nargs="+", default=["train", "test"])
     args = parser.parse_args()
 
@@ -30,24 +31,21 @@ def parse_arguments() -> dict:
 
 
 def get_frame(
-    seq_path: Path, img_id: int, position: np.ndarray, rotmat: np.ndarray
+    img_path: Path, position: np.ndarray, rotmat: np.ndarray, seq_path: Path
 ) -> dict:
     """Create a frame dictionary.
 
     Args:
-        seq_path: path to the sequence that contains the image
-        img_id: image ID read from poses file
+        img_path: path to the image
         position: image position, shape (3,)
         rotmat: image c2w rotation matrix, shape (3, 3)
+        seq_path: path to the sequence that contains the image
 
     Returns:
         dictionary with keys "file_path", "sharpness", "transform_matrix"
     """
-    image_rel_path = f"rgb_matched/frame-color-{str(img_id).zfill(4)}.png"
-    file_path = str(seq_path.relative_to(seq_path.parents[1]) / image_rel_path)
-
-    image_path = seq_path / image_rel_path
-    image = cv2.imread(str(image_path))
+    file_path = str(img_path.relative_to(seq_path.parents[1]))
+    image = cv2.imread(str(img_path))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     sharpness = cv2.Laplacian(gray, cv2.CV_64F).var()
 
@@ -61,22 +59,37 @@ def get_frame(
 
 def main(config: dict) -> None:
     cfg = SimpleNamespace(**config)
-    dataset_path = Path.home() / "data/Ambiguous_ReLoc_Dataset"
-    with open(dataset_path / "camera.yaml") as f:
-        camera_params = yaml.load(f, Loader=yaml.FullLoader)
-    params = {
-        "fl_x": camera_params["camera_matrix"]["data"][0],
-        "fl_y": camera_params["camera_matrix"]["data"][4],
-        "k1": 0.0,
-        "k2": 0.0,
-        "p1": 0.0,
-        "p2": 0.0,
-        "cx": camera_params["camera_matrix"]["data"][2],
-        "cy": camera_params["camera_matrix"]["data"][5],
-        "w": camera_params["image_width"],
-        "h": camera_params["image_height"],
-        "aabb_scale": 4,
-    }
+    dataset_path = Path.home() / "data" / cfg.dataset
+    if cfg.dataset == "AmbiguousReloc":
+        with open(dataset_path / "camera.yaml") as f:
+            camera_params = yaml.load(f, Loader=yaml.FullLoader)
+        params = {
+            "fl_x": camera_params["camera_matrix"]["data"][0],
+            "fl_y": camera_params["camera_matrix"]["data"][4],
+            "k1": 0.0,
+            "k2": 0.0,
+            "p1": 0.0,
+            "p2": 0.0,
+            "cx": camera_params["camera_matrix"]["data"][2],
+            "cy": camera_params["camera_matrix"]["data"][5],
+            "w": camera_params["image_width"],
+            "h": camera_params["image_height"],
+            "aabb_scale": 4,
+        }
+    elif cfg.dataset == "SevenScenes":
+        params = {
+            "fl_x": 585,
+            "fl_y": 585,
+            "k1": 0.0,
+            "k2": 0.0,
+            "p1": 0.0,
+            "p2": 0.0,
+            "cx": 320,
+            "cy": 240,
+            "w": 640,
+            "h": 480,
+            "aabb_scale": 4,
+        }
     params["camera_angle_x"] = np.arctan(params["w"] / (params["fl_x"] * 2)) * 2
     params["camera_angle_y"] = np.arctan(params["h"] / (params["fl_y"] * 2)) * 2
 
@@ -88,14 +101,34 @@ def main(config: dict) -> None:
         for split_path in split_paths
         for seq in next(os.walk(split_path))[1]
     ]
-    poses_paths = [seq_path / f"poses_{seq_path.stem}.txt" for seq_path in seq_paths]
     frames = []
-    for seq_path, poses_path in tqdm(zip(seq_paths, poses_paths), total=len(seq_paths)):
-        _, img_ids, positions, rotmats = read_poses(poses_path)
+    for seq_path in tqdm(seq_paths):
+        if cfg.dataset == "AmbiguousReloc":
+            poses_path = seq_path / f"poses_{seq_path.stem}.txt"
+            _, img_ids, positions, rotmats = read_poses(poses_path)
+            img_paths = [
+                seq_path / f"rgb_matched/frame-color-{str(img_id).zfill(4)}.png"
+                for img_id in img_ids
+            ]
+        elif cfg.dataset == "SevenScenes":
+            img_paths = sorted(seq_path.glob("*.color.png"))
+            pose_paths = [
+                Path(str(img_path).replace("color.png", "pose.txt"))
+                for img_path in img_paths
+            ]
+            positions = []
+            rotmats = []
+            for pose_path in pose_paths:
+                position, rotmat = read_tfmat(pose_path)
+            positions.append(position)
+            rotmats.append(rotmat)
+        else:
+            raise ValueError("Invalid dataset name.")
+
         frames.extend(
             [
-                get_frame(seq_path, img_id, position, rotmat)
-                for img_id, position, rotmat in zip(img_ids, positions, rotmats)
+                get_frame(img_path, position, rotmat, seq_path)
+                for img_path, position, rotmat in zip(img_paths, positions, rotmats)
             ]
         )
     params["frames"] = frames
